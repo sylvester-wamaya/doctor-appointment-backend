dffb468788d3b4bd186643729a536adfb685fc2d
ClintonYounge <clintonyounge18@gmail.com>
2023-10-05 16:24:39 -0400

    Add update endpoint and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+  
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
     head :no_content
-  end  
+  end
 
   private
 
