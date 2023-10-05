06503e646f4583016a0853f729d1148bb4be9701
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-05 23:54:13 +0300

    Create doctors migration

---
+++ db/migrate/20231005193634_create_doctors.rb
@@ -0,0 +1,12 @@
+class CreateDoctors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :specialization
+      t.decimal :consultation_fee
+      t.decimal :prescription_fee
+
+      t.timestamps
+    end
+  end
+end
