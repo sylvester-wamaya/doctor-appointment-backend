e7a276b46bf6621ccfce657bd8f01ff66efc0d5c
ClintonYounge <clintonyounge18@gmail.com>
2023-10-06 12:54:41 -0400

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 User.create(name: 'Junior')
 User.create(name: 'John')
 
-Doctor.create(name: 'clint', specialization: 'surgery', consultation_fee: 4, prescription_fee: 5, photo: "url", user_id: User.first.id)
-Doctor.create(name: 'clinton', specialization: 'surgery', consultation_fee: 4, prescription_fee: 5, photo: "url", user_id: User.first.id)
+Doctor.create(name: 'clint', specialization: 'surgery', consultation_fee: 4, prescription_fee: 5, photo: "url")
+Doctor.create(name: 'clinton', specialization: 'surgery', consultation_fee: 4, prescription_fee: 5, photo: "url")
 
 Appointment.create(doctor_id: Doctor.first.id, user_id: User.first.id, date: DateTime.now, location: 'lagos')
 Appointment.create(doctor_id: Doctor.second.id, user_id: User.first.id, date: DateTime.now, location: 'cambleville')
